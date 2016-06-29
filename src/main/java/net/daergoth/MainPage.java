a2e9f1adc0bdb8efd0ccba76fbc2a564d1ed4d6a
István Lakatos <atyep95@msn.com>
2016-06-29 13:19:14 +0200

    Initial project with Servlet example

---
+++ src/main/java/net/daergoth/MainPage.java
@@ -0,0 +1,32 @@
+package net.daergoth;
+
+import java.io.IOException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MainPage extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public MainPage() {
+        super();
+    }
+
+	public void init(ServletConfig config) throws ServletException {
+	}
+
+	public void destroy() {
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
